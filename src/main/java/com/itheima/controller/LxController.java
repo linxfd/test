be2d096c927f200ff7aa71b125e1fc16d1f1006d
LinXianfang <1300557348@qq.com>
2024-03-08 22:26:50 +0800

    1

--- src/main/java/com/itheima/controller/LxController.java
+++ src/main/java/com/itheima/controller/LxController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LxController {
     @RequestMapping("in")
     public String getLx() {
+        System.out.println("第一次");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
         return "lx";
     }
+
 }
